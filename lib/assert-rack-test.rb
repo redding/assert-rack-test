f01809f69f676e5d3ffd3c2a765aa5cb326eb57b
jcredding <TempestTTU@gmail.com>
2012-11-21 12:01:40 -0600

    Removed the autoload and now include `Assert::Rack::Test`
    This removes the autoload of the version file and just requires it directly. It
    also modifies the pattern of adding rack-test methods to `Assert::Context` and
    instead includes the module `Assert::Rack::Test`. This should be more robust
    for future needs but still provides the same functionality.

--- lib/assert-rack-test.rb
+++ lib/assert-rack-test.rb
@@ -1,14 +1,22 @@
 require 'assert'
 require 'rack/test'
 
+require 'assert-rack-test/version'
+
 module Assert
   module Rack
     module Test
-      autoload :VERSION, "assert-rack-test/version"
+
+      def self.included(klass)
+        klass.class_eval do
+          include ::Rack::Test::Methods
+        end
+      end
+
     end
   end
 end
 
 Assert::Context.class_eval do
-  include Rack::Test::Methods
+  include Assert::Rack::Test
 end
